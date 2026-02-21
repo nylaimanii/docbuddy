0468ba9971f6156eb59f5541fce8ebffaea2038c
Nitya sahithi <nityasahithi@Nityas-MacBook-Air.local>
2026-02-21 15:40:03 -0500

    nitya changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
-import React from "react";
-import DocBuddy from "./components/DocBuddy";
+import { useState, useEffect } from "react";
+import PopScreen from "./components/popScreen";
 
 function App() {
-  return <DocBuddy />;
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      {showSplash && <PopScreen />}
+
+      {!showSplash && (
+        <div className="min-h-screen flex items-center justify-center">
+          <h1>Main App</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
